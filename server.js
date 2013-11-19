9f3e08552650a3c2d45aaa33b5e6c2874c264548
hosys <higehossa@gmail.com>
2013-11-20 08:19:22 +0900

    ストリーミングによるパフォーマンスの最適化
    データを直接pipeする。

--- server.js
+++ server.js
@@ -14,20 +14,37 @@ http.createServer(function (request,response) {
 
 	var lookup = path.basename(decodeURI(request.url)) || 'index.html',
 		f = 'content/' + lookup;
+		// console.log(f);
 	fs.exists(f, function (exists) {
 		if (exists) {
-			cacheAndDeliver(f, function(err, data) {	
-				if (err) {
-					response.writeHead(500);
-					response.end('ServerError!');
-					return;
-				}
-				var headers = {'Content-Type': mimeTypes[path.extname(f)]};
+			var headers = {'Content-Type': mimeTypes[path.extname(f)]+ ';charset=utf-8'};
+			if (cache[f]) {
 				response.writeHead(200, headers);
-				response.end(data);
+				response.end(cache[f].content);
+				return;
+			};
+
+			var s = fs.createReadStream(f).once('open', function () {
+				response.writeHead(200, headers);
+				this.pipe(response);
+
+			}).once('error', function (e) {
+				console.log(e);
+				response.writeHead(500);
+				response.end('サーバエラー');
+			});
+
+			fs.Stats(f, function (err, stats) {
+				var bufferOffset = 0;
+				cache[f] = {content: new Buffer(stats.size)};
+				s.on('data', function (data) {
+					data.copy(cache[f].content, bufferOffset);
+					bufferOffset += data.length;
+				});
 			});
 			return;
 		}
+
 		response.writeHead(404);
 		response.end('ページが見つかりません');
 
@@ -35,5 +52,3 @@ http.createServer(function (request,response) {
 	});
 
 }).listen(8080);
-
-
